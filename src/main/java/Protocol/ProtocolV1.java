3401fd81998cdaa570c0e4ed8fdd6723257bf871
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-05-08 19:32:29 +0200

    Init iteration 3

---
+++ src/main/java/Protocol/ProtocolV1.java
@@ -0,0 +1,14 @@
+
+package Protocol;
+
+/**
+ *
+ */
+public class ProtocolV1 {
+   public static String EXAMPLE = "TEST";
+   
+   public static void messageExample(int value){
+      String test = EXAMPLE + " " + value;
+      // send test
+   }
+}
---
+++ src/main/java/canardage/Serveur.java
@@ -0,0 +1,18 @@
+
+package canardage;
+
+import java.net.ServerSocket;
+
+/**
+ *
+ */
+public class Serveur {
+   ServerSocket serverSocket;
+   
+   private int[] actionCards;
+   private int[][] playerCards;
+   
+   private int nbPlayers;
+
+   
+}
