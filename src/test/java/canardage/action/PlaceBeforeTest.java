3a12910f236a278ba3d3dcbe6b1583bfa95c077e
MiguelPomboDias <miguel.pombodias@heig-vd.ch>
2017-06-13 13:51:47 +0200

    pettes modifs

--- src/test/java/canardage/action/PlaceBeforeTest.java
+++ src/test/java/canardage/action/PlaceBeforeTest.java
@@ -10,8 +10,6 @@ import canardage.Client;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import org.junit.After;
-import org.junit.AfterClass;
 import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
